6c93c9c523a551683664d0cdbbb2bcc2a0799553
kuishandaozhang <kuishandaozhang@example.com>
2020-01-02 17:40:50 +0800

    编辑器工具 批量修改资源名称

---
+++ AssetsBatchRename/Assets/Editor/Tools/AssetSortTool.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class AssetSortTool : MonoBehaviour
+{
+    //调用方法：
+
+　　private void SortFiles()
+    {
+        //string filePath = "E:\\";
+        //DirectoryInfo di = new DirectoryInfo(filePath);
+
+        //FileInfo[] arrFi = di.GetFiles("*.*");
+        //SortAsFileName(ref arrFi);
+
+        //for (int i = 0; i < arrFi.Length; i++)
+        //    Response.Write(arrFi[i].Name + "：<br />");
+    }
+
+    //上面的代码是对 E 盘根目录下的所有文件排序，代码测试通过，可直接调用。
+
+
+    //1、按名称顺序排列
+
+    /// <summary>
+    　　/// C#按文件名排序（顺序）
+    　　/// </summary>
+    　　/// <param name="arrFi">待排序数组</param>
+    private void SortAsFileName(ref FileInfo[] arrFi)
+    {
+        Array.Sort(arrFi, delegate (FileInfo x, FileInfo y) { return x.Name.CompareTo(y.Name); });
+    }
+
+
+    //2、按名称倒序排列
+
+    /// <summary>
+    　　/// C#按文件名排序（倒序）
+    　　/// </summary>
+    　　/// <param name="arrFi">待排序数组</param>
+    private void SortAsFileName_Reverse(ref FileInfo[] arrFi)
+    {
+        Array.Sort(arrFi, delegate (FileInfo x, FileInfo y) { return y.Name.CompareTo(x.Name); });
+    }
+
+    //调用方法跟顺序排列一样，就不举例了。
+
+ 
+
+　　//3、按创建时间顺序排列
+
+    /// <summary>
+　　/// C#按创建时间排序（顺序）
+　　/// </summary>
+　　/// <param name="arrFi">待排序数组</param>
+    private void SortAsFileCreationTime(ref FileInfo[] arrFi)
+    {
+        Array.Sort(arrFi, delegate (FileInfo x, FileInfo y) { return x.CreationTime.CompareTo(y.CreationTime); });
+    }
+
+    //调用方法与上同。
+
+ 
+
+　　//4、按创建时间倒序排列
+
+/// <summary>
+　　/// C#按创建时间排序（倒序）
+　　/// </summary>
+　　/// <param name="arrFi">待排序数组</param>
+  private void SortAsFileCreationTime_Reverse(ref FileInfo[] arrFi)
+    {
+        Array.Sort(arrFi, delegate (FileInfo x, FileInfo y) { return y.CreationTime.CompareTo(x.CreationTime); });
+    }
+
+    //调用方法与上同。
+
+ 
+
+ 
+
+　　//二、C#文件夹排序
+    //调用方法：
+
+　　private void FolderSort()
+    {
+        //string filePath = "E:\\";
+        //DirectoryInfo di = new DirectoryInfo(filePath);
+
+        //DirectoryInfo[] arrDir = di.GetDirectories();
+        //SortAsFolderName(ref arrDir);
+
+        //for (int i = 0; i < arrDir.Length; i++)
+        //    Response.Write(arrDir[i].Name + "：<br />");
+    }
+
+    //上述代码是对 E 盘根目录下的所有文件夹按名称顺序排列，代码也通过 Visual studio 2010 测试。
+
+
+    //1、按文件夹名称顺序排列
+
+    /// <summary>
+    　　/// C#按文件夹名称排序（顺序）
+    　　/// </summary>
+    　　/// <param name="dirs">待排序文件夹数组</param>
+    private void SortAsFolderName(ref DirectoryInfo[] dirs)
+    {
+        Array.Sort(dirs, delegate (DirectoryInfo x, DirectoryInfo y) { return x.Name.CompareTo(y.Name); });
+    }
+
+
+    //2、按文件夹名称倒序排列
+
+    /// <summary>
+    /// C#按文件夹名称排序（倒序）
+    /// </summary>
+    /// <param name="dirs">待排序文件夹数组</param>
+    private void SortAsFolderName_Reverse(ref DirectoryInfo[] dirs)
+    {
+        Array.Sort(dirs, delegate (DirectoryInfo x, DirectoryInfo y) { return y.Name.CompareTo(x.Name); });
+    }
+
+ 
+
+　　//3、按文件夹创建时间顺序排列
+
+/// <summary>
+　　/// C#按文件夹夹创建时间排序（顺序）
+　　/// </summary>
+　　/// <param name="dirs">待排序文件夹数组</param>
+    private void SortAsFolderCreationTime(ref DirectoryInfo[] dirs)
+    {
+        Array.Sort(dirs, delegate (DirectoryInfo x, DirectoryInfo y) { return x.CreationTime.CompareTo(y.CreationTime); });
+    }
+
+
+　　//4、按文件夹创建时间倒序排列
+
+/// <summary>
+　　/// C#按文件夹创建时间排序（倒序）
+　　/// </summary>
+　　/// <param name="dirs">待排序文件夹数组</param>
+    private void SortAsFolderCreationTime_Reverse(ref DirectoryInfo[] dirs)
+    {
+        Array.Sort(dirs, delegate (DirectoryInfo x, DirectoryInfo y) { return y.CreationTime.CompareTo(x.CreationTime); });
+    }
+
+    //如果要按文件或文件夹的最后修改时间排序，方法也是一样的，只需把 CreationTime 改为 LastWriteTime 即可。
+}
---
+++ AssetsBatchRename/Assets/Editor/Tools/BatchRenameTool.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System;
+
+using Object = UnityEngine.Object;
+
+/// <summary>
+/// 资源批量重命名
+/// </summary>
+public class BatchRenameTool : MonoBehaviour
+{
+    private const string UPSET_NAME = "_UPSET_NAME_";   //打乱名字马甲，用于预改名，否则当有文件名称在改名规则内，会因为重名影响文件名称修改
+
+    [MenuItem("Assets/BatchRename")]
+    private static void AssetsBatchRename()
+    {
+        Object[] m_objects = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);//选中的所有对象
+        if (m_objects == null || m_objects.Length <= 0) return;
+
+        SortAsFileName(ref m_objects);              //文件根据名称排序，否则是乱的
+
+        BatchRename(m_objects, UPSET_NAME);         //先打乱名称，防止名称重复改名失败
+
+        string newName = "Tex_";                    //名字马甲
+        int index = 0;                              //序号
+        BatchRename(m_objects, newName, index);     //批量改名
+    }
+
+    /// <summary>
+    /// 批量改名
+    /// </summary>
+    /// <param name="assets">文件</param>
+    /// <param name="newName">新名称马甲</param>
+    /// <param name="startIndex">起始序号</param>
+    private static void BatchRename(Object[] assets, string newName, int startIndex = 0)
+    {
+        foreach (Object item in assets)
+        {
+            if (Path.GetExtension(AssetDatabase.GetAssetPath(item)) != "")  //判断路径是否为空
+            {
+                string path = AssetDatabase.GetAssetPath(item);
+                AssetDatabase.RenameAsset(path, newName + startIndex);
+                startIndex++;
+            }
+        }
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+    /// <summary>
+    /// 将文件按名称排序
+    /// </summary>
+    private static void SortAsFileName(ref Object[] assets)
+    {
+        Array.Sort(assets, delegate (Object x, Object y) { return x.name.CompareTo(y.name); });
+    }
+}
